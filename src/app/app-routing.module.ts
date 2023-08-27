0fe8017e6d109e15eaee05ecec7d7b171ffb2e4a
Ela04 <ga.antunez@duocuc.cl>
2023-08-27 12:00:36 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'practica',
+    loadChildren: () => import('./practica/practica.module').then( m => m.PracticaPageModule)
+  },
 ];
 
 @NgModule({
