0b7cbf09478f3f2796657dfa4a346a9e8ad875dc
Ela04 <ga.antunez@duocuc.cl>
2023-09-08 15:32:31 -0300

    e

--- src/app/crear/crear.module.ts
+++ src/app/crear/crear.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { CrearPageRoutingModule } from './crear-routing.module';
 import { CrearPage } from './crear.page';
 import { HeaderModule } from '../header/header.module';
@@ -11,6 +13,7 @@ import { HeaderModule } from '../header/header.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     CrearPageRoutingModule,
     HeaderModule
   ],
